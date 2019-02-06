627b4b9af7572c47b4736e996e9d0ed588394ca8
Tom Dobbelaere <tom.dobbelaere@outlook.com>
2019-02-06 16:26:34 +0100

    Changed icons

--- app/src/main/java/link/diga/bazbike/GameLocationService.java
+++ app/src/main/java/link/diga/bazbike/GameLocationService.java
@@ -1,7 +1,6 @@
 package link.diga.bazbike;
 
 import android.Manifest;
-import android.app.IntentService;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
@@ -10,114 +9,61 @@ import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.location.Location;
-import android.os.Bundle;
 import android.os.IBinder;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
-import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 
-public class GameLocationService extends Service
-        implements
-        GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener, LocationListener {
-
+public class GameLocationService extends Service {
     public static String TAG = GameLocationService.class.getSimpleName();
-    private GoogleApiClient mGoogleApiClient;
+
+    private final String CHANNEL_ID = "TRACKER";
+    private final String CHANNEL_DESC = "BazBike notifications";
+    private final int NOTIFICATION_ID = 1;
+
     private FusedLocationProviderClient mFusedLocationClient;
     private LocationCallback mLocationCallback;
-
-    private final int FOREGROUND_ID = 123;
-    private final String CHANNEL_ID = "TRACKER";
 
     public GameLocationService() {
         super();
     }
 
-    /**
-     * Creates an IntentService.  Invoked by your subclass's constructor.
-     *
-     * @param name Used to name the worker thread, important only for debugging.
-     */
-    public GameLocationService(String name) {
-        super();
-    }
-
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
-        LocationRequest locationRequest = LocationRequest.create();
-        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        locationRequest.setInterval(4000);
-        locationRequest.setFastestInterval(2000);
-
-        LocationRequest locationRequestLow = LocationRequest.create();
-        locationRequestLow.setPriority(LocationRequest.PRIORITY_LOW_POWER);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) ==
-                PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            mFusedLocationClient.requestLocationUpdates(locationRequest, mLocationCallback, null);
-        } else {
-            Log.d(TAG, "permission denied");
-        }
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-        Log.d(TAG, location.toString());
-    }
-
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        String CHANNEL_ID = "my_channel_01";
-        NotificationChannel channel = new NotificationChannel(CHANNEL_ID,
-                "Channel human readable title",
+        NotificationChannel channel = new NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_DESC,
                 NotificationManager.IMPORTANCE_DEFAULT);
 
         ((NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE)).createNotificationChannel(channel);
 
+        startForeground(NOTIFICATION_ID, getNotification().setContentText("Starting...").build());
+
+        return START_STICKY;
+    }
+
+    private NotificationCompat.Builder getNotification() {
         Intent notificationIntent = new Intent(this, MapsActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
 
-        Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
+        NotificationCompat.Builder notification = new NotificationCompat.Builder(this, CHANNEL_ID)
                 //.setOngoing(true)
                 //.setAutoCancel(false)
                 //.setTicker("Truiton Music Player")
-                .setContentTitle("Hoo")
-                .setContentText("Haa")
-                .setSmallIcon(R.drawable.ic_launcher_foreground)
-                .setContentIntent(pendingIntent)
-                .build();
+                .setContentTitle("Receiving location updates")
+                .setColorized(true)
+                .setColor(ContextCompat.getColor(this, R.color.colorPrimary))
+                .setSmallIcon(R.drawable.ic_stat_tracker)
+                .setContentIntent(pendingIntent);
 
-        startForeground(1, notification);
-
-        return START_STICKY;
+        return notification;
     }
 
     @Override
@@ -139,25 +85,35 @@ public class GameLocationService extends Service
     public void onCreate() {
         super.onCreate();
 
-        if (mGoogleApiClient != null && mGoogleApiClient.isConnected()) {
-            Log.d(TAG, "disconnecting");
-            mGoogleApiClient.disconnect();
-        }
-        Log.d(TAG, "onHandleIntent");
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addApi(LocationServices.API)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .build();
-        mGoogleApiClient.connect();
 
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
         mLocationCallback = new LocationCallback() {
             @Override
             public void onLocationResult(LocationResult locationResult) {
                 super.onLocationResult(locationResult);
+
+                Notification notification = getNotification()
+                        .setContentText(Double.toString(locationResult.getLastLocation().getLatitude())).build();
+
+                NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                mNotificationManager.notify(NOTIFICATION_ID, notification);
+
                 Log.i(TAG, "Location result: " + Double.toString(locationResult.getLastLocation().getLatitude()));
             }
         };
+
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(6000);
+        locationRequest.setFastestInterval(3000);
+
+        LocationRequest locationRequestLow = LocationRequest.create();
+        locationRequestLow.setPriority(LocationRequest.PRIORITY_LOW_POWER);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED) {
+            mFusedLocationClient.requestLocationUpdates(locationRequest, mLocationCallback, null);
+        } else {
+            Log.d(TAG, "permission denied");
+        }
     }
 }
